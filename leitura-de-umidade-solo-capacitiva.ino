392aefb890c44e054acea8c72a974e25088ee716
julio cesar <julio.psk8@hotmail.com>
2023-11-06 21:59:45 -0300

    leitura do sensor no display o led

--- leitura-de-umidade-solo-capacitiva.ino
+++ leitura-de-umidade-solo-capacitiva.ino
@@ -1,35 +1,57 @@
 /*
             SENSOR DE UMIDADE CAPACITIVA 
             DIA : 06-11-2023 
-            TITULO: Leitura simples do sensor
+            TITULO: Leitura simples no display OLed
              
 */
+//===========================================================
+// ------- Bibliotecas ---------------
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
+//===========================================================
+// ------- Objetos ---------------
+Adafruit_SSD1306 display(-1);
+
+//===========================================================
+// ------- Mapeamento de hardwar ---------------
+#define sensor A0
 //-------------------------------------------------
 //------ Variaveis Globais -----
-
-int sensor = A0;
-
-
+int sen;
 //-------------------------------------------------
 //------ configuração inicial -----
 void setup() {
-  pinMode(sensor, INPUT);
-  pinMode(13, OUTPUT);      // aciona led do arduino
-  Serial.begin(9600);
-
-  Serial.print("umidade de solo...");
-
-
-
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+ display.clearDisplay();
+ sen = analogRead(sensor);
 }
 
 void loop() {
-  
-  int rang = analogRead(sensor);
-  Serial.println("Valor: ");
-  Serial.println(rang);
-  delay(250);
+
+   void solo();
+}
+ //===========================================================
+// ------- DESENVOLVIMENTO DAS FUNÇÕES ---------------
+
+void solo()
+{
+
+  display.setTextSize(1); // Fonte do texto
+  display.setTextColor(WHITE); // Cor do texto
+  display.setCursor(0,0); // Posiciona o cursor
+  display.println("UMIDADE SOLO = "); // Imprime mensagem
+  display.setTextSize(1); // Fonte do texto
+  display.setCursor(0,10); // Posiciona o cursor
+  display.println(sen); // Imprime mensagem
+
+  display.display();
+  delay(2000);
+  display.clearDisplay();
 
 
 }
+
+
